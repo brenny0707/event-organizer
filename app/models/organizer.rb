b4d3246a4995963e4ae724281933276e4a04a0e8
Brendan Ko <brenny0707@gmail.com>
2018-02-13 14:29:14 -0500

    Add skeleton methods and finish session_token methods

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,3 +1,49 @@
+# == Schema Information
+#
+# Table name: organizers
+#
+#  id              :integer          not null, primary key
+#  fname           :string           not null
+#  lname           :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class Organizer < ApplicationRecord
-  
+  validates :fname, :lname, :email, :password_digest, :session_token, presence: true
+  validates :email, :session_token, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def is_password?(password)
+
+  end
+
+  def password=(password)
+
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= Organizer.generate_session_token
+  end
 end
