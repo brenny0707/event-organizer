c03fa611e51d9b93317e922da51fe00817113aa2
Brendan Ko <brenny0707@gmail.com>
2018-02-28 12:23:43 -0500

    Add application and sessions controller methods

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,10 +1,23 @@
 class Api::SessionsController < ApplicationController
   def create
-
+    @organizer = Organizer.find_by_credentials(
+      params[:organizer][:email],
+      params[:organizer][:password])
+    if @organizer
+      login(@organizer)
+      render "api/organizers/show" #explicit call since method is in SessionsController but rendering organizer JSON
+    else
+      render json: ["Invalid credentials, try again"], status: 422
+    end
   end
 
   def destroy
-
+    @organizer = current_organizer
+    if @organizer
+      logout
+      render json: {}
+    else
+      render json: ["404, not found"], status: 404
+    end
   end
-
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_organizer, :logged_in?
+
+  def current_organizer
+    @current_organizer ||= Organizer.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_organizer
+  end
+
+  def login(organizer)
+    @current_organizer = organizer
+    session[:session_token] = organizer.reset_session_token!
+  end
+
+  def logout
+    current_organizer.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def require_logged_in
+    redirect_to new_session_url unless logged_in?
+  end
 end
