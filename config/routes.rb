ab2e6a7889f5bb6af701afa03bf677c50aaeb6a0
Brendan Ko <brenny0707@gmail.com>
2018-03-02 12:12:03 -0500

    Hook frontend, add static_pages_controller, 3.5.5 webpack, additional gitignores, root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#root'
+  
   namespace :api, defaults: { format: :json} do
     resource :organizers, only: [:create, :show]
     resource :session, only: [:create, :destroy]
