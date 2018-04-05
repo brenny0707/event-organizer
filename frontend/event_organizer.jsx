2b9b69e4112ecd55289b0d9245f4a12c4e745848
Brendan Ko <brenny0707@gmail.com>
2018-04-05 19:35:47 -0400

    configure working state with null session

--- frontend/event_organizer.jsx
+++ frontend/event_organizer.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Event Org</h1>, root);
+  let store;
+  if (window.currentOrganizer) {
+    const preloadedState = { session: {currentOrganizer: window.currentOrganizer }};
+    delete window.currentOrganizer;
+    store = configureStore(preloadedState);
+    window.dispatch = store.dispatch;
+    window.getState = store.getState;
+  }
+  else{
+    store = configureStore();
+    window.dispatch = store.dispatch;
+    window.getState = store.getState;
+  }
+  ReactDOM.render(<h1 store={store}>Welcome to Event Org</h1>, root);
 });
