a6f3fec9ca552335f49de285de960c3a5ff94812
Brendan Ko <brenny0707@gmail.com>
2018-09-14 18:27:47 -0400

    Add animation to home text

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const Home = (props) => {
   return(
     <div className="home-div">
-      <h1 className="home-welcome-text">Welcome to Event Org</h1>
-      <h2 className="home-subtext">Your assistant to bring order to events again</h2>
+      <div className="home-text-div">
+        <h1 className="home-welcome-text">Welcome to Event Org</h1>
+        <h2 className="home-subtext">Your assistant to bring order to events again</h2>
+      </div>
     </div>
   )
 }
