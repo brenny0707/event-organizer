8251823a2e3adf384483617a1e75fc0c68db9ad6
Brendan Ko <brenny0707@gmail.com>
2018-10-13 07:49:10 -0400

    Add oranizer seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Organizer.destroy_all
+organizer1 = Organizer.create(email: "hallo@moto.com", fname: "Hallo", lname: "Moto", password: "hallomoto")
+
+organizer2 = Organizer.create(email: "sup@sup.com", fname: "sup", lname: "sup", password: "supsup")
+
+organizer3 = Organizer.create(email: "username@password.com", fname: "User", lname: "Name", password: "Password")
+
+demo_oragnizer = Organizer.create(email: "demo@account.com", fname: "Demo", lname: "Account", password: "password")
