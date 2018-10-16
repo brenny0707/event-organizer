0b79923180a1e3b10f27ba5d2e75982b29891aba
Brendan Ko <brenny0707@gmail.com>
2018-10-16 19:05:21 -0400

    Change protect_from_forgery validation for graphQL access

--- frontend/event_organizer.jsx
+++ frontend/event_organizer.jsx
@@ -2,6 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import ApolloClient from "apollo-boost";
+
+import gql from "graphql-tag";
+
+const client = new ApolloClient();
+console.log(client);
+
+client
+  .query({
+    query: gql`
+      {
+        allOrganizers {
+          id
+          fname
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+// client
+//   .query({
+//     query: gql`
+//       {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
