f4061a9b1e2cc828c68fa6ed1b4b746d9af0c540
Brendan Ko <brenny0707@gmail.com>
2018-10-16 19:36:22 -0400

    Test query component

--- frontend/components/greeting.jsx
+++ frontend/components/greeting.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
 
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+
+
+const allOrganizers = () => (
+  <Query
+    query={gql`
+      {
+        allOrganizers {
+          id
+          fname
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return data.allOrganizers.map(({ id, fname }) => (
+        <div key={id}>
+          <p>{`${fname}`}</p>
+        </div>
+      ));
+    }}
+  </Query>
+);
+
 class Greeting extends React.Component {
   constructor(props) {
     super(props);
@@ -8,7 +37,30 @@ class Greeting extends React.Component {
 
   render() {
     return(
-      <div className="greeting-div">Greetings from Greeting!</div>
+      <div className="greeting-div">
+        <p>Greetings from Greeting!</p>
+        <Query
+          query={gql`
+      {
+        allOrganizers {
+          id
+          fname
+        }
+      }
+    `}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
+
+            return data.allOrganizers.map(({ id, fname }) => (
+              <div key={id}>
+                <p>{`${fname}`}</p>
+              </div>
+            ));
+          }}
+        </Query>
+      </div>
     );
   }
 }
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,14 +3,20 @@ import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient();
 
 const Root = ({store}) => {
   return (
-    <Provider store={store}>
-      <HashRouter>
-        <App/>
-      </HashRouter>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </Provider>
+    </ApolloProvider>
   );
 };
 
