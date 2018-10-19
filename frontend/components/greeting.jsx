2f796ef0c718baf4772afbb4c840239b8a46bc27
Brendan Ko <brenny0707@gmail.com>
2018-10-19 10:20:22 -0400

    Create session now returns organizer and token successfully

--- frontend/components/greeting.jsx
+++ frontend/components/greeting.jsx
@@ -3,32 +3,6 @@ import React from 'react';
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
-
-
-const allOrganizers = () => (
-  <Query
-    query={gql`
-      {
-        allOrganizers {
-          id
-          fname
-        }
-      }
-    `}
-  >
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error :(</p>;
-
-      return data.allOrganizers.map(({ id, fname }) => (
-        <div key={id}>
-          <p>{`${fname}`}</p>
-        </div>
-      ));
-    }}
-  </Query>
-);
-
 class Greeting extends React.Component {
   constructor(props) {
     super(props);
